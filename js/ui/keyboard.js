cdb9cf497c7d779ce173d42a709961fc8fc08c34
Dan Winship <danw@gnome.org>
2011-06-02 17:03:52 -0400

    fix warnings about queue_relayout during allocation

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -4,6 +4,7 @@ const Clutter = imports.gi.Clutter;
 const Gdk = imports.gi.Gdk;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 const St = imports.gi.St;
 const Shell = imports.gi.Shell;
 const Caribou = imports.gi.Caribou;
@@ -123,7 +124,7 @@ Keyboard.prototype = {
 
         this.keyboard.connect('notify::active-group', Lang.bind(this, this._onGroupChanged));
         global.screen.connect('monitors-changed', Lang.bind(this, this._reposition));
-        this.actor.connect('allocation-changed', Lang.bind(this, this._reposition));
+        this.actor.connect('allocation-changed', Lang.bind(this, this._queueReposition));
 
         Main.chrome.addActor(this.actor, { visibleInOverview: true,
                                            visibleInFullscreen: true,
@@ -136,6 +137,10 @@ Keyboard.prototype = {
         let primary = global.get_primary_monitor();
         this.actor.x = primary.x;
         this.actor.y = primary.y + primary.height - this.actor.height;
+    },
+
+    _queueReposition: function () {
+        Mainloop.timeout_add(0, Lang.bind(this, function () { this._reposition(); }));
     },
 
     _addKeys: function () {
